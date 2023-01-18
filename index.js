26a293027565da44cf5f2dee5b1750dd52ad73d7
LaupWing <loc_strict@hotmail.com>
2023-01-18 19:03:45 +0100

    udpate

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const { ApolloServer } = require("apollo-server")
+const { resolvers } = require("./schema/resolvers")
+const { typeDefs } = require("./schema/typeDefs")
 
-const server = new ApolloServer()
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(() => {
+   console.log("Your api is running")
+})
